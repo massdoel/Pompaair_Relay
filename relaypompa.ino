e80d5524786f993e19f66acf8386471644217b0f
Abdullah Jiddan <137474892+massdoel@users.noreply.github.com>
2026-04-30 12:14:25 +0700

    Add files via upload

---
+++ relaypompa.ino
@@ -0,0 +1,16 @@
+#define relay 12
+ int dep=7200000; //on pompa selama 2 jam
+ int dej=20000; //delay relay on 20 detik
+
+void setup() {
+ // put your setup code here, to run once:
+ pinMode(relay, OUTPUT);
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(relay, HIGH);
+  delay(dej);
+}
